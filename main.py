a997b46932031cfa2858b16cb63817884cf04016
tergum.engine <basti.novotergum@gmail.com>
2025-12-03 13:27:33 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from app import app  # importiert die Flask-App aus app.py
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
