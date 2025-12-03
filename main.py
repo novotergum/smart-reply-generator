eac3214c0b6d28b302e365ebc8afec8355884695
tergum.engine <basti.novotergum@gmail.com>
2025-12-03 13:23:53 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+from app import app  # importiert die Flask-App aus app.py
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
